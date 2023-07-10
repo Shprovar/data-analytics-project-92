5abb0f027f21dbc89cbcc5057b43a3c3710da247
Shprovar <132303568+Shprovar@users.noreply.github.com>
2023-07-10 16:55:18 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -18,7 +18,7 @@ limit 10;
 --Шаг 5, задание 2 отчет с продавцами, чья выручка ниже средней выручки всех продавцов
 with tab as(
 select concat(e.first_name, ' ', e.last_name) as name,
-avg(p.price * s.quantity) as average_income
+round(avg(p.price * s.quantity),0) as average_income
 from employees e
 inner join sales s
 on e.employee_id = s.sales_person_id
@@ -26,13 +26,14 @@ inner join products p
 on s.product_id = p.product_id
 group by concat(e.first_name, ' ', e.last_name)
 ),
-common_average as(
+common_average_tab as(
 select avg(average_income) as common_average
 from tab
 )
-select name, round(average_income, 0) as average_income
+select name, average_income
 from tab
-where average_income < (select common_average from common_average)
+cross join common_average_tab
+where average_income < common_average
 order by average_income ASC
 ;
 
@@ -51,23 +52,21 @@ order by to_char(s.sale_date, 'ID'), 1
 ;
 
 --Шаг 6, задание 1 количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+
-select age_category,
-count(*) as count
-from (
-select case
+select
+case
 when age >= 16 and age <= 25 then '16-25'
 when age >= 26 and age <= 40 then '26-40'
 else '40+'
-end as age_category
+end as age_category,
+count(*) as count
 from customers
-) as age_groups
 group by age_category
 order by age_category
 ;
 
 --Шаг 6, задание 2  количество покупателей и выручка по месяцам
 select
-concat(to_char(s.sale_date, 'YYYY'), '-', to_char(s.sale_date, 'MM')) as date,
+to_char(s.sale_date, 'YYYY-MM') as date,
 count(distinct c.customer_id) as total_customers,
 sum(p.price * s.quantity) as income
 from customers c
@@ -75,15 +74,15 @@ inner join sales s
 on c.customer_id = s.customer_id
 inner join products p
 on s.product_id = p.product_id
-group by to_char(s.sale_date, 'YYYY'), to_char(s.sale_date, 'MM')
+group by to_char(s.sale_date, 'YYYY-MM')
 order by date
 ;
 
 --Шаг 6, задание 3 покупатели, первая покупка которых пришлась на время проведения специальных акций
-with tab as(
 select
+distinct on (concat(c.first_name, ' ', c.last_name))
 concat(c.first_name, ' ', c.last_name) as customer,
-sale_date, 
+sale_date,
 concat(e.first_name, ' ', e.last_name) as seller
 from customers c
 inner join sales s
@@ -93,8 +92,5 @@ on e.employee_id = s.sales_person_id
 inner join products p
 on s.product_id = p.product_id
 where p.price = 0
-order by c.customer_id asc
-)
-select distinct on (customer) customer, sale_date, seller
-from tab
+order by concat(c.first_name, ' ', c.last_name), c.customer_id, sale_date
 ;
